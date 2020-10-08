de5f297b2ca1a89f21461aaf6ebbf80ac43ccaff
Jorge Julián Acero Lomelí <julianacero98@gmail.com>
2020-10-07 22:49:09 -0500

    meta description added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,10 +8,7 @@ class MyDocument extends Document {
       <Html lang="es">
         <Head>
           <meta charSet="utf-8" />
-          {/* <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          /> */}
+          <meta name="description" content="instalaciones de redes galo" />
           <meta name="theme-color" content="#000000" />
           <link rel="shortcut icon" href={require('assets/img/favicon.png')} />
           <link
