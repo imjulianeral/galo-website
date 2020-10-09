bbf3539c555cb52486eb8a15e0451fd470998b40
Jorge Julián Acero Lomelí <julianacero98@gmail.com>
2020-10-08 21:28:51 -0500

    prop in link fixed

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -18,7 +18,6 @@ class MyDocument extends Document {
           {/* Fonts and icons */}
           <link
             rel="preload"
-            rel="stylesheet"
             type="text/css"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons&display=swap"
           />
